8d72f37386a6645bfc9bf66cbde550391696e667
threadkind <threadkind@gmail.com>
2018-05-01 13:51:06 -0500

    Add functionality to flip all cards in the container

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-const card = document.querySelector('.card');
+//set variable for card container
+const cardContain = document.getElementById('cardsContainer');
 
-card.addEventListener('click', function(){
-	 document.querySelector('.cardFront').classList.toggle('frontFlipped');
-	 document.querySelector('.cardBack').classList.toggle('backFlipped');
+//add event delegation to the card container rather than each individual card
+cardContain.addEventListener('click', function(e){
+	//set variable for the card that was clicked
+	const card = e.target.parentElement;
+	//make sure we are clicking on an actual card and not somewhere else in the card container
+	if (card.classList[0] === 'card') {
+		//set variables for the front and back of the card
+		const front = card.children[0];
+		const back = card.children[1];
+
+		//toggle css classes on the front and back of the cards to make them flip
+		front.classList.toggle('frontFlipped');
+		back.classList.toggle('backFlipped');
+};
+
+
 });
